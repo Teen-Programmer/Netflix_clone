07a6d0ed698baa8e3eacc8fb483189babba38941
ATS527 <truespiritz527@gmail.com>
2021-04-18 20:40:51 +0530

    Completed the whole preview thing

--- lib/widgets/previews.dart
+++ lib/widgets/previews.dart
@@ -36,6 +36,7 @@ class Previews extends StatelessWidget {
                   print(content.name);
                 },
                 child: Stack(
+                  alignment: Alignment.center,
                   children: <Widget>[
                     Container(
                       height: 130,
@@ -43,13 +44,40 @@ class Previews extends StatelessWidget {
                       decoration: BoxDecoration(
                         image: DecorationImage(
                           image: AssetImage(content.imageUrl),
-                          fit: BoxFit.cover,
                         ),
                         shape: BoxShape.circle,
                         border: Border.all(color: content.color, width: 4.0),
                       ),
                       margin: EdgeInsets.symmetric(horizontal: 16),
                     ),
+                    Container(
+                      height: 130,
+                      width: 130,
+                      decoration: BoxDecoration(
+                        gradient: const LinearGradient(
+                          colors: [
+                            Colors.black87,
+                            Colors.black45,
+                            Colors.transparent
+                          ],
+                          stops: [0, 0.25, 1],
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                        ),
+                        shape: BoxShape.circle,
+                        border: Border.all(color: content.color, width: 4.0),
+                      ),
+                      margin: EdgeInsets.symmetric(horizontal: 16),
+                    ),
+                    Positioned(
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: SizedBox(
+                        height: 60,
+                        child: Image.asset(content.titleImageUrl),
+                      ),
+                    )
                   ],
                 ),
               );
